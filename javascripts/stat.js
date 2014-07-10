860b89593bf43d2cccde47f33368242b42e0c08c
haya14busa <hayabusa1419@gmail.com>
2014-07-10 17:18:07 +0900

    Add dot on the line

--- javascripts/stat.js
+++ javascripts/stat.js
@@ -32,9 +32,11 @@
                       .orient("left");
 
         var line = d3.svg.line()
+                     .interpolate("basic")
                      .x(function(d) { return x(d.id); })
                      .y(function(d) { return y(get_members_len(d)); });
 
+        // Draw svg field
         var svg = d3.select("body")
                   .append("svg")
                     .attr("width", width + margin.left + margin.right)
@@ -42,10 +44,10 @@
                   .append("g")
                     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        // x.domain(d3.extent(data, function(d) { return d.id; }));
         x.domain([0, d3.max(data, function(d) { return d.id; })]);
         y.domain([0, d3.max(data, function(d) { return get_members_len(d); })]);
 
+        // Draw x axis
         svg.append("g")
             .attr("class", "x axis")
             .attr("transform", "translate(0," + height + ")")
@@ -58,22 +60,49 @@
             .style("text-anchor", "end")
             ;
 
+        // Draw y axis
         svg.append("g")
             .attr("class", "y axis")
             .call(yAxis)
           .append("text")
-            // .attr("transform", "rotate(-90)")
             .attr("y", 6)
             .attr("dy", ".71em")
             .attr("x", margin.left + 10)
             .style("text-anchor", "middle")
             .text("Participants");
 
+        // Draw line
         svg.append("path")
             .datum(data)
             .attr("class", "line")
             .attr("fill", "steelblue")
             .attr("d", line);
+
+        // Draw dot on the line
+        svg.selectAll("circle")
+            .data(data).enter().append("svg:circle")
+            .attr("class", "circle")
+            .attr({
+                cx: function(d) { return x(d.id); },
+                cy: function(d) { return y(get_members_len(d)); },
+                r: 3,
+                fill: '#ffcc00'
+            })
+            .on('mouseover', function(d) {
+                d3.select(this).attr({
+                    'r': '8'
+                });
+            })
+            .on('mouseout', function(d) {
+                d3.select(this).attr({
+                    'fill': '#ffcc00',
+                    'r': '3'
+                });
+            })
+            .on('click', function(d) {
+                console.log(d);
+            })
+            ;
 
     }
 
