fcaec85adb78fe3a0083383e1ffae01f72ee231f
haya14busa <hayabusa1419@gmail.com>
2014-07-10 20:00:29 +0900

    " -> '

--- javascripts/stat.js
+++ javascripts/stat.js
@@ -16,8 +16,8 @@
     });
 
     // Helper
-    var tooltip = d3.select("body").append("div")
-                    .attr("class", "js-stat-tooltip");
+    var tooltip = d3.select('body').append('div')
+                    .attr('class', 'js-stat-tooltip');
 
     function get_members_len(d) { return d.members.length; }
 
@@ -48,68 +48,68 @@
 
         var xAxis = d3.svg.axis()
                       .scale(x)
-                      .orient("bottom");
+                      .orient('bottom');
 
         var yAxis = d3.svg.axis()
                       .scale(y)
-                      .orient("left");
+                      .orient('left');
 
         var line = d3.svg.line()
-                     .interpolate("basic")
+                     .interpolate('basic')
                      .x(function(d) { return x(d.id); })
                      .y(function(d) { return y(get_members_len(d)); });
 
         // Draw svg field
-        var svg = d3.select("#d3-vimrc-participants-stat")
-                  .append("svg")
-                    .attr("width", width + margin.left + margin.right)
-                    .attr("height", height + margin.top + margin.bottom)
-                  .append("g")
-                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        var svg = d3.select('#d3-vimrc-participants-stat')
+                  .append('svg')
+                    .attr('width', width + margin.left + margin.right)
+                    .attr('height', height + margin.top + margin.bottom)
+                  .append('g')
+                    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
         x.domain([0, d3.max(data, function(d) { return d.id; })]);
         y.domain([0, d3.max(data, function(d) { return get_members_len(d); })]);
 
         // Draw x axis
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
+        svg.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate(0,' + height + ')')
             .call(xAxis)
-          .append("text")
-            .attr("y", 16)
-            .attr("dy", ".71em")
-            .attr("x", width + 10)
-            .text("開催数")
-            .style("text-anchor", "end")
+          .append('text')
+            .attr('y', 16)
+            .attr('dy', '.71em')
+            .attr('x', width + 10)
+            .text('開催数')
+            .style('text-anchor', 'end')
             ;
 
         // Draw y axis
-        svg.append("g")
-            .attr("class", "y axis")
+        svg.append('g')
+            .attr('class', 'y axis')
             .call(yAxis)
-          .append("text")
-            .attr("y", 6)
-            .attr("dy", ".71em")
-            .attr("x", margin.left + 10)
-            .style("text-anchor", "middle")
-            .text("参加者数(人)");
+          .append('text')
+            .attr('y', 6)
+            .attr('dy', '.71em')
+            .attr('x', margin.left + 10)
+            .style('text-anchor', 'middle')
+            .text('参加者数(人)');
 
         // Draw line
-        svg.append("path")
+        svg.append('path')
             .datum(data)
-            .attr("class", "line")
-            .attr("fill", "steelblue")
-            .attr("d", line);
+            .attr('class', 'line')
+            .attr('fill', 'steelblue')
+            .attr('d', line);
 
         // Draw dot on the line
-        svg.selectAll("circle")
+        svg.selectAll('circle')
             .data(data).enter()
-          .append("svg:a")
-            .attr("xlink:href", function(d) {
+          .append('svg:a')
+            .attr('xlink:href', function(d) {
                 return ARCHIVE_URL_TEMPLATE + (d.id + '').lpad(3, '0') + '.html';
             })
-          .append("svg:circle")
-            .attr("class", "circle")
+          .append('svg:circle')
+            .attr('class', 'circle')
             .attr({
                 cx: function(d) { return x(d.id); },
                 cy: function(d) { return y(get_members_len(d)); },
